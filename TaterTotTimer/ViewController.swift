e459177c7129ad3395b7c568e9f2feceebc3777f
Aaron Douglas <astralbodies@gmail.com>
2016-08-09 15:22:30 -0500

    Further moving towards all RxSwift

--- TaterTotTimer/ViewController.swift
+++ TaterTotTimer/ViewController.swift
@@ -18,12 +18,13 @@ class ViewController: UIViewController {
     @IBOutlet var timerFace: UILabel!
     
     let disposeBag = DisposeBag()
-
+    
     var totalNumberOfTots = Variable(5)
+    var timerRunning = Variable(false)
     var timer: NSTimer?
     var targetDate: NSDate?
     var degrees = 0.0
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -44,39 +45,39 @@ class ViewController: UIViewController {
         startStopButton
             .rx_tap
             .subscribeNext {
-                print("Hello")
+                self.timerRunning.value = !self.timerRunning.value
             }
             .addDisposableTo(disposeBag)
         
-    }
-
-    @IBAction func startOrStopTimer() {
-        if let timer = timer {
-            timer.invalidate()
-            self.timer = nil
-            targetDate = nil
-            cancelLocalNotifications()
-            totImage.transform = CGAffineTransformIdentity
-            degrees = 0.0
-            startStopButton.setTitle("Start Timer", forState: .Normal)
-            timerFace.hidden = true
-            totCountStepper.hidden = false
-            totCountLabel.hidden = false
-            return
-        }
-        
-        let dateComponents = NSDateComponents.init()
-        let calendar = NSCalendar.currentCalendar()
-        dateComponents.second = timeForNumberOfTots(totalNumberOfTots.value)
-        targetDate = calendar.dateByAddingComponents(dateComponents, toDate: NSDate.init(), options: [])
-        
-        scheduleLocalNotification(targetDate!)
-        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(ViewController.refreshTotAndTimer), userInfo: nil, repeats: true)
-        startStopButton.setTitle("Stop Timer", forState: .Normal)
-        timerFace.hidden = false
-        totCountStepper.hidden = true
-        totCountLabel.hidden = true
-        refreshTotAndTimer()
+        timerRunning
+            .asObservable()
+            .subscribeNext { value in
+                if !value {
+                    self.startStopButton.setTitle("Start Timer", forState: .Normal)
+                    self.timer?.invalidate()
+                    self.timer = nil
+                    self.targetDate = nil
+                    self.cancelLocalNotifications()
+                    self.totImage.transform = CGAffineTransformIdentity
+                    self.degrees = 0.0
+                    self.timerFace.hidden = true
+                    self.totCountStepper.hidden = false
+                    self.totCountLabel.hidden = false
+                } else {
+                    self.startStopButton.setTitle("Stop Timer", forState: .Normal)
+                    let dateComponents = NSDateComponents.init()
+                    let calendar = NSCalendar.currentCalendar()
+                    dateComponents.second = self.timeForNumberOfTots(self.totalNumberOfTots.value)
+                    self.targetDate = calendar.dateByAddingComponents(dateComponents, toDate: NSDate.init(), options: [])
+                    
+                    self.scheduleLocalNotification(self.targetDate!)
+                    self.timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(ViewController.refreshTotAndTimer), userInfo: nil, repeats: true)
+                    self.timerFace.hidden = false
+                    self.totCountStepper.hidden = true
+                    self.totCountLabel.hidden = true
+                }
+            }
+            .addDisposableTo(disposeBag)
     }
     
     func refreshTotAndTimer() {
@@ -84,7 +85,7 @@ class ViewController: UIViewController {
         let dateComponents = calendar.components([.Minute, .Second], fromDate: NSDate(), toDate: targetDate!, options: [])
         
         guard targetDate!.timeIntervalSinceReferenceDate > NSDate().timeIntervalSinceReferenceDate else {
-            startOrStopTimer()
+            self.timerRunning.value = false
             return
         }
         
@@ -123,6 +124,6 @@ class ViewController: UIViewController {
     }
     
     
-
+    
 }
 
